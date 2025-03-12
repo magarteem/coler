09d133d19d7e2b9a33640cad3d6110b17732a133
magarteem <fmf311@mail.ru>
2025-03-12 17:18:38 +0200

    rrr

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,2 @@
 export { default } from "next-auth/middleware";
-export const config = { matcher: ["/", "/subscription", "/tariffs"] };
+export const config = { matcher: [ "/subscription", "/tariffs"] };
